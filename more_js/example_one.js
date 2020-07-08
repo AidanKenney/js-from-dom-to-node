ace59930821bfc5a9e202be9cbbef75473c46f51
Aidan Kenney <kenneyaidan@gmail.com>
2020-07-08 17:27:59 +0000

    Done.

--- more_js/example_one.js
+++ more_js/example_one.js
@@ -1,5 +1,8 @@
 let person1 = "Peter"
 let person2 = "Withnail"
+let person3 = 'Damien'
 
 console.log( 'Hi, how are you, ' + person2 + '?' )
-console.log( 'Oh, I am doing just fine, ' + person1 + ', thanks!' )
+console.log( 'Oh, I am doing just fine, ' + person1 + ', thanks!')
+console.log( 'What about me?')
+console.log( 'Oh, hi,'  + person3 )
